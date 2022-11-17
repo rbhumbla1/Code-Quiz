a903f63791ecd1e1a90e035afc6748d4056d8842
Rima Bhumbla <105192284+rbhumbla1@users.noreply.github.com>
2022-11-16 16:22:31 -0800

    added documentation to script.js, README file and added some screenshots

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # Code-Quiz
 Repository for Module 4 Challenge - Code Quiz using HTML, CSS, JavaScript and Web APIS
+
+## Description
+Code Quiz runs a timed Javascript quiz and displays the high scorea fo al the participants.  Here are the features of this program:
+
+1. At the start of the Quiz, a message is displayed informing the user about the rule that any wrong answer will result in time detected from the timer.  it also displays the Start button.
+2. When the user clicks the Start button:
+    * The timer starts at 75 seconds on the upper right corner of the header
+    * The first question is displayes with 4 button choices to select he answer from.     
+3. When the user selects one of the 4 buttons:
+    * A message is displayed in the footer - "Correct!" if the answer selected is correct and "Wrong!" if the choice is incorrect.
+    * If this question was not the last question and the timer is not zero, the next question is displayed.
+    * 10 seconds are detected from the timer if the answer choice is wrong.   None is detected for a correct answer.
+4. If the question is the last one or the timer has reached zero:
+    * The timer displays "Done" on header
+    * A message "All Done!" is displayed.
+    * The final score which is the time left on the timer)
+    * An input box for the user to type in their initials
+    * A Submit button is displayed 
+5. When the uer type in their initials and click on the Submit button:
+    * A list of high scores with initials and scores from previous sessions is displayed
+    * Initials and score from this sessions are displayed appended to the end.
+    * If this is the first round, only score from thisround is displayed.
+    * A "Go Back" button is displayed
+    * A "Clear High Score" button is displayed
+6. When the "Go Back" button is pressed, the quiz start again with message described in step #1 above and Start button.
+7. When "Clear High Score" is pressed, nothing happens.  
+8. At any point in the game, if the uer clicks on "View High Score" link on top left corner, the list of high scores so far from all sessions is displayed in an alert popup.
+
+
+## Mock Up
+
+![Screenshots of the quiz](./assets/images/start.JPG, ./assets/images/question.JPG, ./assets/images/score.JPG, ./assets/images/highScore.JPG)
+
+Demo of the project:
+![Code Quiz demo](./assets/images/Password-Generator-Demo.gif)
+
+
+## Usage
+You can access:
+1. the file in GitHub repository: https://github.com/rbhumbla1/Code-Quiz
+2. the application using this URL: https://rbhumbla1.github.io/Code-Quiz
+
+## License
+None
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,25 @@
+//Creatign a wrapper element to process all clicks on the page.
 var wrapper = document.querySelector(".wrapper");
+//start Button
 var startBtn = null;
+// variable for putting initial message with Start button and then the multiple choice questions dynamically
 var initCard = document.querySelector("#init-card");
+//Timer display
 var timer = document.querySelector("#timerDisp");
+//variable to to display result of each question as correct or wrong
 var result = document.querySelector("#result");
+//variable to store user entered initials
 var initials = null;
+//Variable to keep track of timer.  Each round get 75 seconds
 var timeLeft = 75;
+//Variable to keep track of number of questions
 var numQues = 0;
+//Variable to keep track of the score
 var score = 0;
+//Array to store (initials, score) pair in Local storage
 var scoreList = [];
 
-// var question = {
-//     ques:"",
-//     btn1:"",
-//     btn2:"",
-//     btn3:"",
-//     btn4:"",
-//     answer:""
-// };
-
+//array to store all the questions, theirs choices of answers and correct answer
 var questionList = [
     {ques:"Commonly used data types do NOT include: ", btn1:"1. strings", btn2:"2. boolean", btn3:"3. alerts", btn4:"4. numbers", ans:"3. alerts"},
     {ques:"The condition in an if / else statement is enclosed with ________.", btn1:"1. quotes", btn2:"2. curly brackets", btn3:"3. parantheses", btn4:"4. square brackets", ans:"3. parantheses"},
@@ -26,68 +28,57 @@ var questionList = [
     {ques:"A very useful tool used during development and debugging for printing content is: ", btn1:"1. Javascript", btn2:"2. terminal/bash", btn3:"3. for loops", btn4:"4. console.log", ans:"4. console.log"}
 ]
 
-console.log(initCard);
-console.log(startBtn);
-
+//Timer function  - it is executed when Start button is pressed
 function startTimer() {
-    // Sets interval in variable
     var timerInterval = setInterval(function () {
-
 
         if (timeLeft === 0) {
             // Stops execution of action at set interval
             clearInterval(timerInterval);
-            // Calls function to create and append image
+            // Calls function to save the score and intials in gloval variables
             saveResults();
         } else {
+            //Start timer if timeLeft is not 0 and run the quiz
             timeLeft--;
             timer.textContent = "Timer: " + timeLeft;
             initCard.setAttribute("class", "textProps justifyLeft");
-            startQuiz();
+            runQuiz();
         }
 
     }, 1000);
 
 }
 
-//Function to start the quiz
-function startQuiz() {
-    console.log("startQuiz..." + numQues)
+//Function to run the quiz
+function runQuiz() {
+    //pick the question to be displayed from the array of questions
     var question =questionList[numQues];
-    //initCard.innerHTML = "What all is included in a web page?<br><button class=\"btn\" id =\"btn1\">1. HTML</button><br><button class=\"btn\" id =\"btn2\">2. CSS</button><br><button class= \"btn\" id =\"btn3\">3. JS</button><br><button class=\"btn\" id =\"btn4\">4. All of the above."
+    
+    //Display the question in initCard paragraph
     initCard.innerHTML = question.ques +"<br><button class=\"btn\" id =\"btn1\">" +question.btn1+ "</button><br><button class=\"btn\" id =\"btn2\">" +question.btn2+ "</button><br><button class= \"btn\" id =\"btn3\">" +question.btn3+ "</button><br><button class=\"btn\" id =\"btn4\">" +question.btn4;
 }
 
-//Function to calculate if the user selected correct response
-function getResults(btnValue) {
-    console.log("get result..." + numQues);
-    var ans = false;
-    var question = questionList[numQues];
-    if(question.ans === btnValue)
-     ans = true;
-    else
-     ans = false;
 
-    numQues++;
-    return ans;
-}
-
-// Function to save results in local storage using players initial
+// Function to save users score and initial - this is called when Timer is done or all the questions are done and timer is set to zero.
 function saveResults() {
-    console.log("timer or question done");
+    
     timer.textContent = "Timer: Done";
 
-    //get user initials
+    //First display the final score and create the input on the page where the user can enter their initials
     initCard.innerHTML = "<b>All Done!</b><br>Your final score is " + score + ".<br>Enter Initials: <input id = \"initials\" type=\"text\"> <button id=\"Submit\" class=\"btn\">Submit</button>"
+    //get user initials
     initials = document.querySelector("#initials");
 }
 
+//Get the list of Initials and score from Local Storage to display high scores from previous runs
+//if link = true, we need to create a display string for alert popup when View High Score lin is clicked
+//If link = false, we need to createa string to display high score on the card in the apge.
 function getScoreListString(link) {
+    //get stored initial/score pair from local storage
     var storedList = JSON.parse(localStorage.getItem("scoreList"));
     var values = "";
 
     for (var i = 0; i < storedList.length; i++) {
-        console.log("sow res = " + storedList[i].initials + " - " + storedList[i].score);
         var y = i+1;
         if(!link)
          values += "<span>" + y + ". " + storedList[i].initials + " - " + storedList[i].score + "</span><br>";
@@ -96,112 +87,162 @@ function getScoreListString(link) {
 
     }
 
-    console.log("values = " + values);
     return values;
 }
 
-//Function to show results list
+//Function to calculate if the user selected correct response
+function getResults(btnValue) {
+   
+    var ans = false;
+    var question = questionList[numQues];
+    //checks the text of button clicked in multiple choice to the stores correct answer for that question in questions array
+    if(question.ans === btnValue)
+     ans = true;
+    else
+     ans = false;
+
+    //increment numQues to go to next question
+    numQues++;
+
+    return ans;
+}
+
+//Function to show results list in the card on the page
 function showResults() {
+
     var values = getScoreListString(false);
 
     initCard.innerHTML = "<b>High Scores:</b><br>" + values + "<br> <button id=\"goBack\" class=\"btn\">Go Back</button><button id=\"clearScores\" class=\"btn\">Clear High Scores</button>";
 }
 
+//main Event listener for warpper element - it will parse all the clicks for links and various buttons on the page
 wrapper.addEventListener("click", function (event) {
     var element = event.target;
     var answer = false;
     console.log(element);
     event.preventDefault();
 
+    if (element.innerHTML === "View High Scores") {  //View High Scores
+        console.log("View high score clicked");
 
-    if (element.innerHTML === "View High Scores") {
-        console.log("view high score clicked");
         var values = getScoreListString(true);
         var newValues = values.replace(/<br>/g, "\n");
         alert(newValues);
 
-    } else if (element.innerHTML === "Start") {
-        console.log("Start button.." + element.innerHTML);
+    } else if (element.innerHTML === "Start") { //Start Button
+        console.log("Start button clicked");
+
+        //start the timer when start button is clicked
         startTimer();
 
-    } else if (element.innerHTML === "Submit") {
+    } else if (element.innerHTML === "Submit") { //Submit Button
+
         console.log("Submit clicked");
-        //object to store scores in local storage
-        console.log("score = " + score + "initial = " + initials.textContent + " or init = " + initials.value.trim());
+
+        //userScore object to store scores in local storage
         var userScore = {
             initials: initials.value.trim(),
             score: score
         };
+
+        //add the latest userScore to the ScoreList
         scoreList[scoreList.length] = userScore;
-        // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
+
+        //weite scoreList to local storage
         localStorage.setItem("scoreList", JSON.stringify(scoreList));
+
+        //show all the scores stored in local storage so far
         showResults();
 
-    } else if (element.innerHTML === "Go Back") {
+    } else if (element.innerHTML === "Go Back") { //Go back
+
+        console.log("Go Back clicked");
+
+        //This will go back to the beginning and sets all the variables to their initial value before reloading the page
         timeLeft = 75;
         timer.textContent = "Timer: " + timeLeft;
         initCard.setAttribute("class", "textProps");
         result.textContent = "";
         numQues = 0;
         score = 0;
-       // init();
+
+       // For some reason calling init(); if not working properly all the time so using reload function to get to correct page
        location.reload();
 
-    } else if (element.innerHTML === "Clear high Scores") {
-        scoreList.length = 0;
-        scoreList = [];
+    } else if (element.innerHTML === "Clear high Scores") {  //Clear High Score Button
+ 
+        console.log("Clear High Score clicked");
+
+       //empty out the scoreList
+        scoreList.splice(0, scoreList.length);
+        //store in local storage
         localStorage.setItem("scoreList", JSON.stringify(scoreList));
+        //clear out the display on page
         initCard.innerHTML = "<b>High Scores:</b><br><span></span>\n <button id=\"goBack\" class=\"btn\">Go Back</button><button id=\"clearScores\" class=\"btn\">Clear High Scores</button>";
 
-    } else if (element.innerHTML !== "Start") {
-        console.log("button.." + element.innerHTML);
+    } else if (element.innerHTML !== "Start") {       //Any of the Answer Button 
+
+        console.log("One of the answer button clicked");
+
+        //Return if all questions are done
         if(numQues === 5)
             return;
+        
+        //check if answer is correct or wrong
         answer = getResults(element.innerHTML);
+
+        //answer is correct
         if (answer === true) {
             result.textContent = "Correct!";
+            //If last question, stoptimer and save score
             if (numQues === 5) {
                 //save timer as score before setting it to 0
                 score = timeLeft;
                 timeLeft = 0;
                 console.log("numQues = " + numQues)
-            } else{
-                startQuiz();
+            } else{       //if more question, keep running the quiz 
+                runQuiz();
             }
-        
-        } else {
+        } else { //answer is wrong
             result.textContent = "Wrong!";
+
+            //deduct 10s from timer for wrong answer
             timeLeft -= 10;
+            //save timeLeft as score
             score = timeLeft;
+
+            //set score to zero if negative value
             if (score < 0)
                 score = 0;
-            console.log("wrong..." + timeLeft + "..." + numQues)
+            
+            //If time is negative or all questions donw, then set timer to 0 to stop the quiz
             if (timeLeft < 0 || numQues === 5) {
                 timeLeft = 0;
-                console.log("settng timer 0");
             } else{
-                startQuiz();
+                runQuiz();  //otherwise keep running the quiz
             }
         }
 
     } else {
-        console.log("something else clicked");
+        console.log("Ignore redundant clicks.");
     }
 })
 
 
-
+//Main fucntion
+//It setups up the start message
+//Also initialize the scoreList for the session with any initial/scores pairs stored in local storage from previous sessions
 function init() {
     initCard.innerHTML = "Click Start button to start the timed quiz. Remember a wrong answer will detect time from the timer.<br><button id=\"start\" class\=\"btn\">Start</button>";
     startBtn = document.querySelector("#start");
-    console.log(startBtn);
 
-    //get stored scored
+    //get stored scores
     var storedList = JSON.parse(localStorage.getItem("scoreList"));
     if (storedList !== null) {
         scoreList = storedList;
     }
 }
 
+//Call init
 init();
 
